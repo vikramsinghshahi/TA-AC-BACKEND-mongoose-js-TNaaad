dca4fad8d3b8c08494eddb43e03a5176ce861cf3
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-08 17:39:43 +0530

    block-BNaacb

---
+++ block-BNaacb/sample/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/sample', (err) => {
+  console.log(err ? err : 'mongoose is connected');
+});
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
+
+app.listen(3000, () => {
+  console.log('server is listening at 3k');
+});
