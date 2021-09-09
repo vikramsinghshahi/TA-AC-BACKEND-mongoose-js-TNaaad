796d770a79f7869eef6aa681e04d8a60aba3a7f2
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-09 11:59:28 +0530

    update

--- block-BNaacn/sample/models/user.js
+++ block-BNaacn/sample/models/user.js
@@ -2,13 +2,17 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name: String,
-  email: { type: String, lowercase: true },
-  age: { type: Number, default: 0 },
-  password: { type: String, minlength: 5 },
-  createdAt: { type: Date, default: new Date() },
-});
+var userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, lowercase: true },
+    age: { type: Number, default: 0 },
+    password: { type: String, minlength: 5 },
+    createdAt: { type: Date, default: new Date() },
+    favourites: [String],
+  },
+  { timestamps: true }
+);
 
 var User = mongoose.model('User', userSchema);
 
